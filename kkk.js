6c66d31d7e0442972c5079ab0d11637ba406d5d1
zky111111 <2537510863@qq.com>
2024-09-10 21:53:41 +0800

    Update kkk.js

--- kkk.js
+++ kkk.js
@@ -1,3 +1,13 @@
 function showAlert() {
     alert('你好，这是一个简单的网站！');
 }
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "xcm.jpg") {
+        myImage.setAttribute("src", "xcm2.jpg");
+    } else {
+        myImage.setAttribute("src", "xcm.jpg");
+    }
+};
